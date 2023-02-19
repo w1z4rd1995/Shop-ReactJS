670b01d907908e769ecbd96f285c8bb8429bca53
w1z4rd1995 <pawa_10@mail.ru>
2023-02-19 23:25:18 +0300

    improved css of cart product

--- src/components/OneCart.js
+++ src/components/OneCart.js
@@ -8,14 +8,18 @@ export const OneCart = observer((props) => {
             <div className="imageCart">
                 <img src={props.Items.image} />
             </div>
-            <div className="cartTitle">{props.Items.title}</div>
-            <div className="cartRating">
-                <Rating value={props.Items.rating.rate} readOnly />
+            <div className="cartTitle">
+                <div className="cartTitleStyle">{props.Items.title}</div>
+                <div className="cartRating">
+                    <Rating value={props.Items.rating.rate} readOnly />
+                </div>
             </div>
-            <div className="cartPrice">{props.Items.price} $</div>
+
             <div className="cartItemQuantity">кол-во</div>
             <div className="cartButtons">
+                <div className="cartPrice">{props.Items.price} $</div>
                 <input
+                    className="deleteButtonStyle"
                     type="button"
                     value="Удалить"
                     onClick={() => {
