ba6c155551abf157c7d6c18d18c5d67b6a6233e0
w1z4rd1995 <pawa_10@mail.ru>
2023-04-04 23:13:57 +0300

    working with footer content

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import { CategoryProducts } from "./Pages/CategoryProducts";
 import { Products } from "./components/Products";
 import { Cart } from "./Pages/Cart";
 import { OneProductPage } from "./Pages/OneProductPage";
+import footerlogo from "./components/images/footerLogo.PNG";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CallIcon from "@mui/icons-material/Call";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const App = observer(() => {
     return (
@@ -40,11 +47,62 @@ const App = observer(() => {
             </div>
             <div className="footer">
                 <div className="footerContent">
-                    <NavLink to="/">Главная</NavLink>
-                    <NavLink to="/catalog">Каталог</NavLink>
-                    <NavLink to="/products">Все товары</NavLink>
-                    <NavLink to="/cart">Корзина</NavLink>
-                    <NavLink to="/contacts">Контакты</NavLink>
+                    <div className="footerLogo">
+                        <img className="logoStyle" src={footerlogo}></img>
+                    </div>
+                    <div className="footerPages">
+                        <div>
+                            <h2>Страницы</h2>
+                        </div>
+                        <div className="footerNav">
+                            <NavLink to="/">Главная</NavLink>
+                            <NavLink to="/catalog">Каталог</NavLink>
+                            <NavLink to="/products">Все товары</NavLink>
+                            <NavLink to="/cart">Корзина</NavLink>
+                            <NavLink to="/contacts">Контакты</NavLink>
+                        </div>
+                    </div>
+                    <div className="footerButton">
+                        <a className="footerButtonStyle">Связаться с нами </a>
+                    </div>
+                    <div className="footerContacts">
+                        <div>
+                            <h2>Контакты</h2>
+                        </div>
+                        <div className="footerContactsInfo">
+                            <div className="footerLocation">
+                                <div>
+                                    <LocationOnIcon />
+                                </div>
+                                <div>
+                                    123456, Россия, г. Москва, ул. Степанова, д.
+                                    100{" "}
+                                </div>
+                            </div>
+                            <div className="footerPhone">
+                                <div>
+                                    <CallIcon />
+                                </div>
+                                <div>+7(495)4958800</div>
+                            </div>
+                            <div className="footerMail">
+                                <div>
+                                    <MailOutlineIcon />
+                                </div>
+                                <div>sobaka@mail.ru</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footerMoreInfo">
+                    <div className="footerIcons">
+                        <TelegramIcon fontSize="large" />
+                        <FacebookIcon fontSize="large" />
+                        <TwitterIcon fontSize="large" />
+                    </div>
+                    <div className="footerText">
+                        Copyright &copy; 2023. All rights reserved.
+                    </div>
                 </div>
             </div>
         </div>
