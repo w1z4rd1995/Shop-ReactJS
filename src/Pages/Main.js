c15e363460332048c00c7b95e4056b745e01b4c3
w1z4rd1995 <pawa_10@mail.ru>
2023-04-27 20:59:26 +0300

    added item description on hover at slider product

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,9 +2,21 @@ import { observer } from "mobx-react-lite";
 import { store } from "../Stores/AppStore";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "antd";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Main = observer(() => {
+    const navigate = useNavigate();
+    const [isImageHover, setIsImageHover] = useState(false);
+
+    const onMouseEnter = () => {
+        setIsImageHover(true);
+    };
+
+    const onMouseLeave = () => {
+        setIsImageHover(false);
+    };
+
     useEffect(() => {
         if (store.IsReady === false) {
             store.fetchData();
@@ -27,10 +39,23 @@ export const Main = observer(() => {
     return (
         store.allImages && (
             <div className="imageSliderContainer">
-                <Carousel autoplay autoplaySpeed={3000} effect="fade">
-                    {store.allImages.map((item, i) => (
-                        <div className="hui" key={i}>
-                            <img className="imageSlider" src={item} />
+                <Carousel autoplay autoplaySpeed={2000} effect="fade">
+                    {store.ApiData.map((item, i) => (
+                        <div className="imageContainer" key={i}>
+                            <div
+                                className="hoverImage"
+                                onMouseEnter={onMouseEnter}
+                                onMouseLeave={onMouseLeave}
+                                onClick={() => navigate(`/products/${item.id}`)}
+                            >
+                                <img className="imageSlider" src={item.image} />
+
+                                {isImageHover && (
+                                    <div className="imageDesc">
+                                        {item.description}
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     ))}
                 </Carousel>
